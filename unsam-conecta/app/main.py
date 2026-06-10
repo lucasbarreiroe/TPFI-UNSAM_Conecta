283facdab708f01cd8678d6f50eda7ac2e1b5ff3
EriGorosito <engorosito@estudiantes.unsam.edu.ar>
2026-06-10 20:05:46 -0300

    prueba render

--- unsam-conecta/app/main.py
+++ unsam-conecta/app/main.py
@@ -74,4 +74,5 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
