56ae0475d5c8e4610d180437ee226c7c4cb44146
EriGorosito <engorosito@estudiantes.unsam.edu.ar>
2026-06-11 09:50:47 -0300

    Arreglo registros

--- unsam-conecta/app/api/v1/endpoints/auth.py
+++ unsam-conecta/app/api/v1/endpoints/auth.py
@@ -57,8 +57,14 @@ async def register(
     await db.refresh(db_user)
 
     # Crear token y enviar correo en SEGUNDO PLANO
+    #verification_token = create_verification_token(email=db_user.email)
+    #background_tasks.add_task(send_verification_email, db_user.email, verification_token)
+
+    # Crear token y enviar correo DIRECTAMENTE (bloqueante)
     verification_token = create_verification_token(email=db_user.email)
-    background_tasks.add_task(send_verification_email, db_user.email, verification_token)
+    
+    # Ejecutamos la función directamente aquí
+    send_verification_email(db_user.email, verification_token)
 
     return db_user
 
