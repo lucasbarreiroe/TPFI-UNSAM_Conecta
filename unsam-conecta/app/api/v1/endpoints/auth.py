d04f05096159884a05c3044220d25ed4cc8279f0
EriGorosito <engorosito@estudiantes.unsam.edu.ar>
2026-06-12 12:58:17 -0300

    Arreglos mail

--- unsam-conecta/app/api/v1/endpoints/auth.py
+++ unsam-conecta/app/api/v1/endpoints/auth.py
@@ -56,40 +56,8 @@ async def register(
     await db.commit()
     await db.refresh(db_user)
 
-    # Crear token y enviar correo en SEGUNDO PLANO
-    # verification_token = create_verification_token(email=db_user.email)
-    # background_tasks.add_task(send_verification_email, db_user.email, verification_token)
-
-    # # Crear token y enviar correo de forma ASÍNCRONA (No bloquea la respuesta)
-    # verification_token = create_verification_token(email=db_user.email)
-    
-    # # Agregamos el 'await' aquí:
-    # await send_verification_email(db_user.email, verification_token)
-
     return db_user
 
-# # NUEVO: Endpoint para verificar desde el correo
-# @router.get("/verify")
-# async def verify_email(token: str, db: AsyncSession = Depends(get_db)):
-#     try:
-#         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-#         email: str = payload.get("sub")
-#         if payload.get("type") != "email_verification":
-#             raise HTTPException(status_code=400, detail="Token inválido")
-#     except JWTError:
-#         raise HTTPException(status_code=400, detail="El enlace ha expirado o es inválido")
-
-#     result = await db.execute(select(User).where(User.email == email))
-#     user = result.scalar_one_or_none()
-
-#     if not user:
-#         raise HTTPException(status_code=404, detail="Usuario no encontrado")
-
-#     user.is_verified = True
-#     await db.commit()
-
-#     # Redirige al login del frontend avisando que fue exitoso
-#     return RedirectResponse(url="/login?verified=true")
 
 @router.post("/login", response_model=Token)
 async def login(
@@ -104,13 +72,6 @@ async def login(
             detail="Correo o contraseña incorrectos.",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    
-    # # NUEVO: Bloqueo si no está verificado
-    # if not user.is_verified:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_403_FORBIDDEN, 
-    #         detail="Por favor, revisa tu correo y verifica tu cuenta antes de iniciar sesión."
-    #     )
     
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
